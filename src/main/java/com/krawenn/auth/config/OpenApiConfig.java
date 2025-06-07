baf8f56246eda2559dc574d8ae6d31ceeae53387
onurokyay <onurokyay1@gmail.com>
2025-06-07 18:10:49 +0300

    OpenApiConfig server

---
+++ src/main/java/com/krawenn/auth/config/OpenApiConfig.java
@@ -0,0 +1,42 @@
+package com.krawenn.auth.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springdoc.core.customizers.OpenApiCustomizer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Auth API")
+                        .version("v1")
+                        .description("JWT-based authentication API"))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components().addSecuritySchemes(securitySchemeName,
+                        new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+
+    @Bean
+    public OpenApiCustomizer openApiCustomizer(@Value("${swagger.gateway-url}") String gatewayUrl) {
+        return openApi -> openApi.setServers(List.of(new Server().url(gatewayUrl)));
+    }
+
+}
